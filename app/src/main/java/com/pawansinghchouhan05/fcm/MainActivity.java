c1f5b58a7fd382c97abde9ce0889c292ad9dc788
pawan singh chauhan <pawansinghchouhan05@gmail.com>
2016-05-23 12:55:36 +0530

    FCM Client Side implementation

---
+++ app/src/main/java/com/pawansinghchouhan05/fcm/MainActivity.java
@@ -0,0 +1,40 @@
+package com.pawansinghchouhan05.fcm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button subscribeButton = (Button) findViewById(R.id.subscribeButton);
+        subscribeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // [START subscribe_topics]
+                FirebaseMessaging.getInstance().subscribeToTopic("news");
+                Log.e(TAG, "Subscribed to news topic");
+                // [END subscribe_topics]
+            }
+        });
+
+        Button logTokenButton = (Button) findViewById(R.id.logTokenButton);
+        logTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "InstanceID token: " + FirebaseInstanceId.getInstance().getToken());
+            }
+        });
+    }
+}
